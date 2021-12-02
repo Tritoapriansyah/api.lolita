546823aabed0416e81f87ea429b7b2b4b3831621
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-12-02 23:09:06 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'isi dengan url database mongodb';
+var url = 'mongodb+srv://Trito:23042006@test.orfuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 try {
-if(url == 'isi dengan url database mongodb') throw console.log(color('Configurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://Trito:23042006@test.orfuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority') throw console.log(color('Configurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, Zeeone','green'))
+  console.log(color('Connected correctly to server, Console','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
