7d71091e6f932811cc722d6797aaab71245f08a6
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-12-06 22:24:22 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
-const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'Trito' // ubah jadi nama lu
+const listkey = ["inikey"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
