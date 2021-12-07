e39bf4c4abfc956d2a9051778a30b75a0ffa261b
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-12-07 08:53:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require("dotenv").config({ path: "./config.env" });
 var express = require('express'),
     cors = require('cors'),
-    secure = require('ssl-express-www');
+    secure = require('ssl-express-www'),
 const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
+var { color } = require('./lib/color.js');
+const errorHandler = require("./middleware/error");
+const mongoose = require("mongoose")
+
+(async function () {
+    await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
+    console.log("Database connected.");
+})();
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
 
 var app = express()
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+app.use(errorHandler);
 app.enable('trust proxy');
 app.set("json spaces",2)
 app.use(cors())
